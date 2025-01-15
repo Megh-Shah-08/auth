968d6a0af576594195d96d43623135651081b700
Megh Shah <meghshah810@gmail.com>
2025-01-15 17:52:31 +0530

    App Completed

--- ServerBased/backend/index.js
+++ ServerBased/backend/index.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const PORT = 4000;
 const connectToMongoDB = require("./db");
-const mongoose = require("mongoose");
-const User = require("./User");
-const bcrypt = require("bcrypt");
+const AuthUser = require("./models/AuthUser");
+const bcrypt = require("bcryptjs");
 
 app.use(express.json());
+app.use(cors());
 connectToMongoDB();
 
 app.get("/", (req, res) => {
@@ -15,48 +16,52 @@ app.get("/", (req, res) => {
 
 app.post("/register", async (req, res) => {
   const { name, email, password } = req.body;
-  const existingUser = await User.findOne({ email });
-  if (existingUser) {
+  const existingUser = await AuthUser.findOne({ email });
+  if (existingUser != null) {
     return res.status(400).json({ message: "Email already exists" });
   }
   const saltRounds = 10;
   const passwordHash = await bcrypt.hash(password, saltRounds);
-  const newUser = {
-    name,
+  const newUser = new AuthUser({
+    username: name,
     email,
-    passwordHash,
-  };
+    password: passwordHash,
+  });
   try {
     const savedUser = await newUser.save();
     res
       .status(201)
-      .json({ message: "User registered successfully", user: savedUser });
+      .json({ message: "User registered successfully", user: savedUser.username });
   } catch (err) {
     res.status(500).json({ message: "Error registering user", error: err });
   }
 });
 
-app.post("/login",async (req,res)=>{
-    const { email, password } = req.body;
-    const foundUser = await User.findOne({ email });
-    if(foundUser){
-        const passwordCompare = await bcrypt.compare(foundUser.password,password);
-        if(passwordCompare){
-            res
-            .status(201)
-            .json({ message: "User login successfull", user: foundUser });      
-        }    
-        else{
-            res
-            .status(201)
-            .json({ message: "Incorrect Credentials" });      
-        }
-    }else{
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const foundUser = await AuthUser.findOne({ email });
+
+    if (foundUser) {
+      const passwordCompare = await bcrypt.compare(
+        password,
+        foundUser.password
+      );
+      if (passwordCompare) {
         res
-        .status(201)
-        .json({ message: "User not found", user: foundUser});  
+          .status(200)
+          .json({ message: "User login successful", user: foundUser.username });
+      } else {
+        res.status(401).json({ message: "Incorrect Credentials" });
+      }
+    } else {
+      res.status(404).json({ message: "User not found" });
     }
-})
+  } catch (err) {
+    res.status(500).json({ message: "An error occurred", error: err.message });
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`APP LISTENING ON PORT: ${PORT}`);
--- ServerBased/backend/models/AuthUser.js
+++ ServerBased/backend/models/AuthUser.js
@@ -39,6 +39,6 @@ userSchema.pre('save', function (next) {
 });
 
 // Create a model from the schema
-const User = mongoose.model('User', userSchema);
+const AuthUser = mongoose.model('User', userSchema);
 
-module.exports = User;
+module.exports = AuthUser;
